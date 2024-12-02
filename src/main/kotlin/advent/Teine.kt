dd4d03ff98f715fb6e3934dc11ce3cb140af351a
Maido Käära <maido@lightyear.com>
2024-12-02 19:54:08 +0200

    Reduce

--- src/main/kotlin/advent/Teine.kt
+++ src/main/kotlin/advent/Teine.kt
@@ -1,18 +1,21 @@
 package advent
 
 import kotlin.Int.Companion.MAX_VALUE
-import kotlin.Int.Companion.MIN_VALUE
 
 class Teine : Advent {
+
+    data class Coordinates(val xMin: Int = MAX_VALUE, val xMax: Int = 0, val yMin: Int = MAX_VALUE, val yMax: Int = 0)
+
     override fun task(input: List<String>): String {
-        val xMin = input.minOf {
-            it.foldIndexed(MAX_VALUE) { i, acc, char -> if (char == 'X' && acc > i) i else acc }
-        }
-        val xMax = input.maxOf {
-            it.foldIndexed(MIN_VALUE) { i, acc, char -> if (char == 'X' && acc < i) i else acc }
-        }
-        val yMax = input.foldIndexed(MAX_VALUE) { j, acc, line -> if (line.contains("X")) j else acc }
-        val yMin = input.foldRightIndexed(MIN_VALUE) { j, line, acc -> if (line.contains("X")) j else acc }
-        return (((xMax + 1 - xMin + 1) + (yMax + 1 - yMin + 1)) * 2).toString()
+        return input.foldIndexed(Coordinates()) { rowNr, result, line ->
+            (line.indexOf('X') to line.lastIndexOf('X')).let { (first, last) ->
+                result.copy(
+                    yMin = if (first >= 0) minOf(rowNr, result.yMin) else result.yMin,
+                    yMax = if (first >= 0) maxOf(rowNr, result.yMax) else result.yMax,
+                    xMin = if (first >= 0) minOf(result.xMin, first) else result.xMin,
+                    xMax = if (last >= 0) maxOf(result.xMax, last) else result.xMax
+                )
+            }
+        }.let { (it.xMax - it.xMin + it.yMax - it.yMin + 4) * 2 }.toString()
     }
 }
