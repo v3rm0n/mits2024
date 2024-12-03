9d2207522965e50bd7cb5ed67064ef3fa5387e35
Maido Käära <maido@lightyear.com>
2024-12-03 20:32:27 +0200

    Kolmas

--- src/main/kotlin/Advent.kt
+++ src/main/kotlin/Advent.kt
@@ -1,6 +1,6 @@
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import kotlin.Int.Companion.MAX_VALUE
+import kotlin.math.pow
 
 class Advent {
 
@@ -28,20 +28,24 @@ class Advent {
         assertEquals("56.98", esimene(Inputs.esimene.lines()))
     }
 
-    data class Coordinates(val xMin: Int = MAX_VALUE, val xMax: Int = 0, val yMin: Int = MAX_VALUE, val yMax: Int = 0)
+    data class Coordinates(val xMin: Int? = null, val xMax: Int? = null, val yMin: Int? = null, val yMax: Int? = null) {
+        fun boundingBox() = (xMax!! - xMin!! + yMax!! - yMin!! + 4) * 2
+    }
 
     fun teine(input: List<String>): String {
         return input.foldIndexed(Coordinates()) { rowNr, result, line ->
-            (line.indexOf('X') to line.lastIndexOf('X')).let { (first, last) ->
-                if (first < 0) result else
-                    result.copy(
-                        yMin = minOf(rowNr, result.yMin),
-                        yMax = maxOf(rowNr, result.yMax),
-                        xMin = minOf(result.xMin, first),
-                        xMax = maxOf(result.xMax, last)
-                    )
-            }
-        }.let { (it.xMax - it.xMin + it.yMax - it.yMin + 4) * 2 }.toString()
+            line.indexOf('X').takeIf { it >= 0 }?.let { xMin ->
+                val xMax = line.lastIndexOf('X')
+                result.copy(
+                    yMin = result.yMin ?: rowNr,
+                    yMax = rowNr,
+                    xMin = minOf(result.xMin ?: xMin, xMin),
+                    xMax = maxOf(result.xMax ?: xMax, xMax)
+                )
+            } ?: result
+        }
+            .boundingBox()
+            .toString()
     }
 
     @Test
@@ -64,5 +68,39 @@ class Advent {
     @Test
     fun teineVastus() {
         assertEquals("2998", teine(Inputs.teine.lines()))
+    }
+
+    fun kolmas(input: List<String>): String {
+        return input.first().split(" ")
+            .let { letters ->
+                (2.0.pow(letters.count()) - letters.zip(letters.count() - 1 downTo 0)
+                    .sumOf { (letter, depth) -> if (letter == "v") 2.0.pow(depth) else 0.0 })
+                    .toInt()
+                    .toString()
+            }
+    }
+
+    @Test
+    fun kolmasTest1() {
+        val input = "v p v v"
+        assertEquals("5", kolmas(input.split("\n")))
+    }
+
+
+    @Test
+    fun kolmasTest2() {
+        val input = "v v p p p p p v p v"
+        assertEquals("251", kolmas(input.split("\n")))
+    }
+
+    @Test
+    fun kolmasTest3() {
+        val input = "p v v v p v p p"
+        assertEquals("140", kolmas(input.split("\n")))
+    }
+
+    @Test
+    fun kolmasVastus() {
+        assertEquals("2147483647", kolmas(Inputs.kolmas.lines()))
     }
 }
--- src/main/kotlin/Inputs.kt
+++ src/main/kotlin/Inputs.kt
@@ -1,5 +1,6 @@
 object Inputs {
     val esimene = readFile("esimene.txt")
     val teine = readFile("teine.txt")
+    val kolmas = readFile("kolmas.txt")
     private fun readFile(name: String) = Inputs::class.java.getResource(name).readText()
 }
